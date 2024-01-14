70c5fdb4881eaf9fe521e6736dda81855af65079
jadenday20 <day18007@byui.edu>
2024-01-13 18:40:52 -0700

    Added more functionality including an item delete button

---
+++ index.js
@@ -0,0 +1,55 @@
+var sortedList = [];
+let listTag = document.getElementById("sort-list");
+
+let input = document.getElementById("NewItem");
+let addButton = document.getElementById("addButton");
+
+window.onload = function () {
+  input.focus();
+};
+
+input.addEventListener("keypress", function (event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter" && input.value != "") {
+    addButton.click();
+  }
+});
+
+addButton.addEventListener("click", function () {
+  if (input.value != "") {
+    //reset the list element
+    listTag.innerHTML = "";
+    //add the new item to the array
+    sortedList.push(input.value);
+    //sort the list
+    sortedList.sort();
+    //reset the input field so the user can enter something else
+    input.value = null;
+    //recreate the list element with list items sorted properly
+    for (i = 0; i < sortedList.length; i++) {
+      if (sortedList[i] != undefined) {
+        addNewItem(listTag, sortedList[i], i);
+      }
+    }
+  }
+});
+
+function addNewItem(list, newItem, id) {
+  let listElement = document.createElement("div");
+  listElement.id = `li${id}`;
+  listElement.innerHTML = `
+      <button class="deleteButton" onclick="deleteItem(${id})">X</button>
+      <li class="sortedListElement">
+      ${newItem}
+      </li>`;
+  list.appendChild(listElement);
+}
+
+function deleteItem(id) {
+  let elementToRemove = document.getElementById(`li${id}`);
+  if (elementToRemove) {
+    elementToRemove.remove();
+    delete sortedList[id];
+  }
+  input.focus();
+}
